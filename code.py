3eb4a1777047bba448c24539014247ed99c7ecf0
EReich <smartbutt10@gmail.com>
2022-06-15 14:53:09 -0400

    updated library for fakePIL and blinky lights, added startup animation

--- code.py
+++ code.py
@@ -5,6 +5,11 @@ import adafruit_framebuf
 import pyRTOS
 import rtc
 import time
+import random
+
+#constants for blinky light mode
+BRIGHTNESS = 50  # CONFIGURABLE: LED brightness, 0 (off) to 255 (max)
+PERCENT = 33  # CONFIGURABLE: amount of 'on' LEDs, 0 (none) to 100 (all)
 
 
 c = rtc.RTC()
@@ -32,10 +37,32 @@ hr_button.switch_to_input(pull=Pull.DOWN)
 min_button = DigitalInOut(board.GP14)
 min_button.switch_to_input(pull=Pull.DOWN)
 
+class FakePILImage: #used in the blinky lights portion of the code
+    """Minimal class meant to simulate a small subset of a Python PIL image,
+    so we can pass it to the IS31FL3731 image() function later. THIS IS THE
+    DIRTY POOL PART OF THE CODE, because CircuitPython doesn't have PIL,
+    it's too much to handle. That image() function is normally meant for
+    robust "desktop" Python, using the Blinka package...but it's still
+    present (but normally goes unused) in CircuitPython. Having worked with
+    that library source, I know exactly what object members its looking for,
+    and can fake a minimal set here...BUT THIS MAY BREAK IF THE LIBRARY OR
+    PIL CHANGES!"""
+
+    def __init__(self):
+        self.mode = "L"  # Grayscale mode in PIL
+        self.size = (16, 9)  # 16x9 pixels
+        self.pixels = bytearray(16 * 9)  # Pixel buffer
+
+    def tobytes(self):
+        """IS31 lib requests image pixels this way, more dirty pool."""
+        return self.pixels
+
+
+
 def DisplayTask(self):
     #start with init code
     #DEFINE AN INITIAL MODE 0 THAT CHANGES WITH THE MODE CHANGE LATER
-    mode = 0
+    mode = 2
     
     # Four matrices on each bus, for a total of eight display definitions
     DISPLAY = [
@@ -83,7 +110,7 @@ def DisplayTask(self):
                 text[6] = inTime[6]
                 text[7] = inTime[7]
             elif msg.type == SENT_MODE:
-                mode = msg.mode
+                mode = msg.message
         ### End Mailbox
         
         #MODE 2 BLOCK USING AN IF
@@ -91,21 +118,21 @@ def DisplayTask(self):
             IMAGE = FakePILImage()  # Instantiate fake PIL image object
             FRAME_INDEX = 0  # Double-buffering frame index
 
-            while True:
-                # Draw to each display's "back" frame buffer
-                for disp in DISPLAY:
-                    for pixel in range(0, 16 * 9):  # Randomize each pixel
-                        IMAGE.pixels[pixel] = BRIGHTNESS if random.randint(1, 100) <= PERCENT else 0
-                    # Here's the function that we're NOT supposed to call in
-                    # CircuitPython, but is still present. This writes the pixel
-                    # data to the display's back buffer. Pass along our "fake" PIL
-                    # image and it accepts it.
-                    disp.image(IMAGE, frame=FRAME_INDEX)
+            
+            # Draw to each display's "back" frame buffer
+            for disp in DISPLAY:
+                for pixel in range(0, 16 * 9):  # Randomize each pixel
+                   IMAGE.pixels[pixel] = BRIGHTNESS if random.randint(1, 100) <= PERCENT else 0
+                # Here's the function that we're NOT supposed to call in
+                # CircuitPython, but is still present. This writes the pixel
+                # data to the display's back buffer. Pass along our "fake" PIL
+                # image and it accepts it.
+                disp.image(IMAGE, frame=FRAME_INDEX)
 
-                # Then quickly flip all matrix display buffers to FRAME_INDEX
-                for disp in DISPLAY:
-                    disp.frame(FRAME_INDEX, show=True)
-                FRAME_INDEX ^= 1  # Swap buffers
+            # Then quickly flip all matrix display buffers to FRAME_INDEX
+            for disp in DISPLAY:
+                disp.frame(FRAME_INDEX, show=True)
+            FRAME_INDEX ^= 1  # Swap buffers
         
         #MODE 0 AND 1 BLOCK USING AN ELSE
         else:
@@ -157,8 +184,8 @@ def DisplayTask(self):
                 disp.frame(frame, show=True)
             frame = 0 if frame else 1
         
-        # DONT INCLUDE THIS IN THE IF BLOCK IT NEEDS TO BE AVAILABLE EVERY LOOP OR ELSE EVERYTHING DIES
-        yield [pyRTOS.timeout(0.01)] 
+        
+        yield [pyRTOS.timeout(0.001)] 
 
 def get_tens(num): #simple function used in the later TimeTask thread to get the 10's place of numbers for output
     pos_nums = []
@@ -299,7 +326,8 @@ def ButtonTask(self):
     prev_min_in = 0
     
     yield #first yield, everything after this is the main thread loop
-    
+    yield [pyRTOS.timeout(2)]
+    self.send(pyRTOS.Message(SENT_MODE, self, "display_task", mode))
     while True:
         
         #First the mode toggling state machine
